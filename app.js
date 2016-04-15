9c2c9e1c6965282d58eb7ab1006da9bf5217612c
Shane Henning <shanerhenning@gmail.com>
2016-04-15 14:19:36 -0700

    finished problem 3

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 function sum(numOne, numTwo){
   var sum = numOne + numTwo;
-  return '"The sum of ' + numOne + ' and ' + numTwo + ' is ' + sum + '."';
+  // console.log('"The sum of ' + numOne + ' and ' + numTwo + ' is ' + sum + '."');
+  return sum;
 }
 
 function multiply(numOne, numTwo){
   var multiply = numOne * numTwo;
-  return '"The product of ' + numOne + ' and ' + numTwo + ' is ' + multiply + '."';
+  return multiply;
+  // console.log('"The product of ' + numOne + ' and ' + numTwo + ' is ' + multiply + '."');
+}
+
+function sumAndMultiply(numOne, numTwo, numThree){
+  var sumTwo = sum(sum(numOne, numTwo), numThree);
+  var multTwo = multiply(multiply(numOne, numTwo), numThree);
+  var numArray = [sumTwo, multTwo];
+  console.log('"' + numOne + ' and ' + numTwo + ' and ' + numThree + ' sums to ' + numArray[0] + '."');
+  console.log('"' + numOne + ' and ' + numTwo + ' and ' + numThree + ' have a product of ' + numArray[1] + '."');
 }
